92abafd7b3d1c862f7da16e034cdaf66968271a5
Eric Song <146142807+ericsonghi@users.noreply.github.com>
2025-04-11 00:34:48 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -9,13 +9,13 @@ public class Digits
 	digitList = new ArrayList<Integer>();
    		if(num == 0){
      		digitList.add(0);
-	while(num > 0){
+		}
+		while(num > 0){
      		digitList.add(0, num % 10);
      		num /= 10;
 	    
 	        }
             }
-	}
 
 	public boolean isStrictlyIncreasing(){
 	for(int i = 0; i < digitList.size() - 1; i++){
